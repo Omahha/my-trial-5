0bbd688bce9f398133f460347f50a07c13df1f6b
Khanit <omahha7@gmail.com>
2018-10-02 17:20:34 +0900

    push

--- push.php
+++ push.php
@@ -2,8 +2,8 @@
 
 require "./vendor/autoload.php";
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('weCV1UB0jNHZOHIjosN0Cz5kCJ/mkiudeXUsYV7lBDtClTi3PmMDfhp9S3OMZJ3/K5fAtQFE0eE14KwVBojZKab9gqsurO81WYb7t73zvN1fRVdtfLuulXRxWH0441g1DfFZeb3Kdco5d6sfbABn1QdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '2bc86a87dc5a71bf884791a3b52e67b8']);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('8J5+vrtRTwWVcnUL1R1RZeLycFPJ6GMHn3QJLtzz1KrRypTNGymM7M/Y5FZ+0Mai5AuF0mw5O/XQ0p6oT/U0cOx2pwtsiYrFj5g2wd0Mc0kBoIOHQYYprw3qX3nj9ow/ZZ0zGfHIV/bqIJjmPM8elwdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '111cabd3f125c2905df4a6c4f14b1b72']);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->pushMessage('C93e6dd2670c62d4c028ebbaf23051667', $textMessageBuilder);
