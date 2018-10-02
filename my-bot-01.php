b51ef63e1a9fe0c79c2ebc989dd87a80e66e52e2
Khanit <omahha7@gmail.com>
2018-10-02 11:38:43 +0900

    add my-bot-01

---
+++ my-bot-01.php
@@ -0,0 +1,4 @@
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('weCV1UB0jNHZOHIjosN0Cz5kCJ/mkiudeXUsYV7lBDtClTi3PmMDfhp9S3OMZJ3/K5fAtQFE0eE14KwVBojZKab9gqsurO81WYb7t73zvN1fRVdtfLuulXRxWH0441g1DfFZeb3Kdco5d6sfbABn1QdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '2bc86a87dc5a71bf884791a3b52e67b8']);
+
+$response = $bot->replyText('<reply token>', 'hello!');
